81212b7051945892aad7196f8b23ca957c4b4527
papaeye <papaeye@gmail.com>
2015-06-19 19:16:55 +0900

    Fix runexamples.sh

--- runexamples.sh
+++ runexamples.sh
@@ -2,6 +2,6 @@
 
 cd $(dirname $0)/examples
 
-docker run --rm -v $PWD:/data papaeye/texlive:japanese               -u -l japanese && mv japanese.pdf japanese-noembed.pdf
-docker run --rm -v $PWD:/data papaeye/texlive:japanese-ipaex         -u -l japanese && mv japanese.pdf japanese-ipaex.pdf
-docker run --rm -v $PWD:/data papaeye/texlive:japanese-hiragino-pron -u -l japanese && mv japanese.pdf japanese-hiragino-pron.pdf
+docker run --rm -v $PWD:/data papaeye/texlive:japanese               ptex2pdf -u -l japanese && mv japanese.pdf japanese-noembed.pdf
+docker run --rm -v $PWD:/data papaeye/texlive:japanese-ipaex         ptex2pdf -u -l japanese && mv japanese.pdf japanese-ipaex.pdf
+docker run --rm -v $PWD:/data papaeye/texlive:japanese-hiragino-pron ptex2pdf -u -l japanese && mv japanese.pdf japanese-hiragino-pron.pdf
