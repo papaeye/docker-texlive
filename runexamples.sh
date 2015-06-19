5e660c34f7ab94dd04999c04ef1d31bb9addb87d
papaeye <papaeye@gmail.com>
2015-06-19 21:50:28 +0900

    Update Docker repository for japanese image

--- runexamples.sh
+++ runexamples.sh
@@ -2,6 +2,6 @@
 
 cd $(dirname $0)/examples
 
-docker run --rm -v $PWD:/data papaeye/texlive:japanese               ptex2pdf -u -l japanese && mv japanese.pdf japanese-noembed.pdf
-docker run --rm -v $PWD:/data papaeye/texlive:japanese-ipaex         ptex2pdf -u -l japanese && mv japanese.pdf japanese-ipaex.pdf
-docker run --rm -v $PWD:/data papaeye/texlive:japanese-hiragino-pron ptex2pdf -u -l japanese && mv japanese.pdf japanese-hiragino-pron.pdf
+docker run --rm -v $PWD:/data papaeye/texlive-ja               ptex2pdf -u -l japanese && mv japanese.pdf japanese-noembed.pdf
+docker run --rm -v $PWD:/data papaeye/texlive-ja:ipaex         ptex2pdf -u -l japanese && mv japanese.pdf japanese-ipaex.pdf
+docker run --rm -v $PWD:/data papaeye/texlive-ja:hiragino-pron ptex2pdf -u -l japanese && mv japanese.pdf japanese-hiragino-pron.pdf
