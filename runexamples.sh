7b1c3bc58ad8334edb1e5514097b863c720fea52
papaeye <papaeye@gmail.com>
2015-07-05 23:39:53 +0900

    Remove old attempts

--- runexamples.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-cd $(dirname $0)/examples
-
-docker run --rm -v $PWD:/data papaeye/texlive-ja               ptex2pdf -u -l japanese && mv japanese.pdf japanese-noembed.pdf
-docker run --rm -v $PWD:/data papaeye/texlive-ja:ipaex         ptex2pdf -u -l japanese && mv japanese.pdf japanese-ipaex.pdf
-docker run --rm -v $PWD:/data papaeye/texlive-ja:hiragino-pron ptex2pdf -u -l japanese && mv japanese.pdf japanese-hiragino-pron.pdf
